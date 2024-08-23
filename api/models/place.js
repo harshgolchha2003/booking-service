7d6b1d016cfdb945d8c9f3e958889f8eb39884db
harshgolchha2003 <harshgolchha48@gmail.com>
2024-08-23 13:11:29 +0530

    schema-for-adding-places

--- api/models/place.js
+++ api/models/place.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
-const PlaceSchema= new mongoose.Schema({
-    owner:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+const {Schema}=mongoose;
+const PlaceSchema= new Schema({
+    owner:{type:Schema.Types.ObjectId,ref:'User'},
     title: String,
     address: String,
     photos: [String],
@@ -12,6 +13,6 @@ const PlaceSchema= new mongoose.Schema({
     maxGuest: Number,
 });
 
-const PlaceModel= mongoose.Model('Place',PlaceSchema);
+const PlaceModel= mongoose.model('Place',PlaceSchema);
 
-model.exports=PlaceModel;
+module.exports=PlaceModel;
